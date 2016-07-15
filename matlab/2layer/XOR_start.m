e63ce9654b32f1f7173728260dec76ffa012afef
Alessandro Rossi <rossi111@unisi.it>
2016-07-15 16:58:42 +0200

    Update XOR_start.m

--- matlab/2layer/XOR_start.m
+++ matlab/2layer/XOR_start.m
@@ -17,17 +17,57 @@ n = nnInit(hiddenUnits,inputSize,outputSize);
 %   training
 n = nnTrain(n,X,Y,epochsOfTraining,learningRate);
 
-% plot performance
+
+
+% % % % % plot Loss of network in time
+
 figure;
 
+% plot Loss of network in time
 subplot(1,2,1);
 plot(n.Loss);
 xlabel('Epochs of Training','FontSize',14)
-xlabel('MSE','FontSize',14)
+ylabel('MSE','FontSize',14)
 title('Training Error','FontSize',16)
 
+
+% plot separation surface
 subplot(1,2,2);
-plot(n.Accuracy);
-xlabel('Epochs of Training','FontSize',14)
-xlabel('AccuracyE','FontSize',14)
-title('Training Classification Accuracy','FontSize',16)
+
+bound = [-.5,2;-1,1.5]; % axis bound
+step = .3; % number of evaluation point
+
+% generating space grid
+[xp1,xp2] = meshgrid(bound(1,1):step:bound(1,2),bound(2,1):step:bound(2,2));
+
+% evaluation on space grid
+f = zeros(size(xp1));
+for i=1:size(xp1,1)
+    for j=1:size(xp1,2)
+        n = nnEval(n,[xp1(i,j);xp2(i,j)]);
+        f(i,j) = n.o;
+    end
+end
+
+pcolor(xp1,xp2,f); % plot of evaluation color
+shading interp; % removing gridding from plot
+colormap(jet); % setting colormap
+hold on;
+contour(xp1,xp2,f,[.5,.5],'LineWidth',2,'Color','k'); % drawing separation curve
+% drawing data points 
+scatter(X(1,[1,4]),X(2,[1,4]),200,'o','filled','MarkerEdgeColor','k','MarkerFaceColor','w');
+scatter(X(1,[2,3]),X(2,[2,3]),200,'d','filled','MarkerEdgeColor','k','MarkerFaceColor','w');
+% labeling data points
+c = {'X_1','X_2','X_3','X_4'};
+dx = [-.15, -.15, .1, .1];
+dy = [-.1, .1, -.1, .1];
+text(X(1,:)+dx, X(2,:)+dy, c, 'FontSize',14);
+colorbar;
+
+% plot labels
+xlabel('X_1','FontSize',14)
+ylabel('X_2','FontSize',14)
+
+title('Separation Surfaces','FontSize',16);
+h = legend({'Prediction','Classes Bound','Class 0','Class 1'},'Location','SouthEast');
+set(h,'FontSize',14);
